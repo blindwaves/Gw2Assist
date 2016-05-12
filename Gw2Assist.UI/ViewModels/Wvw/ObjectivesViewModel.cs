444e0f03a59496244145b9631e917cee6b889b3a
blindwaves <blindwaves@hotmail.com>
2016-05-12 20:49:43 +0800

    Initial attempt to do the WvW objectives UI.

--- Gw2Assist.UI/ViewModels/Wvw/ObjectivesViewModel.cs
+++ Gw2Assist.UI/ViewModels/Wvw/ObjectivesViewModel.cs
@@ -2,6 +2,7 @@
 
 using Caliburn.Micro;
 
+using Gw2Assist.Anet.GuildWars2.Api.V2.Core.Models;
 using Gw2Assist.Anet.GuildWars2.Api.V2.Core.Models.Wvw;
 using Gw2Assist.Core.Scheduler.Jobs;
 
@@ -20,11 +21,51 @@ namespace Gw2Assist.UI.ViewModels.Wvw
             }
         }
 
+        private List<World> worlds;
+        public List<World> Worlds
+        {
+            get { return this.worlds; }
+            set
+            {
+                this.worlds = value;
+                NotifyOfPropertyChange(() => this.Worlds);
+            }
+        }
+
+        private int selectedWorld;
+        public int SelectedWorld
+        {
+            get { return this.selectedWorld; }
+            set
+            {
+                this.selectedWorld = value;
+                NotifyOfPropertyChange(() => this.SelectedWorld);
+            }
+        }
+
         private int mapId;
         private readonly IEventAggregator eventAggregator;
 
         public ObjectivesViewModel(IEventAggregator eventAggregator)
         {
+            // TODO:
+            // 1. Grab the world the user is in.
+            // 2. Grab the matches for the world.
+            // 3. Then compare which map is the user in.
+            // 4. Then grab the objectives in that map.
+            // 5. last flipped, count down, yada yada.
+            //
+            // 3 - 5 should be done by scheduler.
+
+            this.Worlds = new List<World>();
+            var cacheWorlds = Core.Cache.Repository.Instance.GetContainer<Core.Cache.Containers.Gw2Worlds>();
+            foreach (var world in cacheWorlds.Contents)
+            {
+                this.Worlds.Add(world.Value);
+            }
+
+            this.SelectedWorld = 1006;
+
             this.eventAggregator = eventAggregator;
             this.eventAggregator.Subscribe(this);
         }
@@ -43,14 +84,25 @@ namespace Gw2Assist.UI.ViewModels.Wvw
                 }
             }
 
+            /** TODO: TEST **/
+            var currentId = 94;
+            if (this.mapId != currentId)
+            {
+                this.mapId = currentId;
+
+                mapChanged = true;
+            }
+            /** **/
+
             if (mapChanged)
             {
-                var cacheWvWObjectives = new Core.Cache.Containers.WvwObjectives();
+                var cacheWvWObjectives = Core.Cache.Repository.Instance.GetContainer<Core.Cache.Containers.Gw2WvwObjectives>();
 
                 if (cacheWvWObjectives.Contents.ContainsKey(this.mapId))
                 {
                     this.Objectives = cacheWvWObjectives.Contents[this.mapId];
                 }
+                System.Diagnostics.Debug.WriteLine(this.Objectives.Count);
             }
         }
     }
