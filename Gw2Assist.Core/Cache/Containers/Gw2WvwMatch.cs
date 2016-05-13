04a8e17ba096cacb7ec5a11d7e36cfc12af1b208
blindwaves <blindwaves@hotmail.com>
2016-05-13 12:07:31 +0800

    Prep cache for a single match.

---
+++ Gw2Assist.Core/Cache/Containers/Gw2WvwMatch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Gw2ApiRequest = Gw2Assist.Anet.GuildWars2.Api.V2.Requests;
+
+namespace Gw2Assist.Core.Cache.Containers
+{
+    public class Gw2WvwMatch : Interfaces.IContainer
+    {
+        public Models.Wvw.Match Contents { get; private set; }
+        public string FileFullPath { get; private set; }
+        public string Name { get { return this.GetType().Name; } }
+
+        public async Task Create(string storagePath)
+        {
+            // TODO: Implement settings and grab the world.
+            var settingsWorldId = 1005;
+            // TODO: NULL check for settings, for new app.
+
+            var identifiers = new Dictionary<string, IEnumerable<string>>();
+            identifiers.Add("world", new List<string>() { settingsWorldId.ToString() });
+
+            var request = new Gw2ApiRequest.Wvw.Matches();
+            request.Identifiers = identifiers;
+
+            this.Contents = await Anet.GuildWars2.Api.V2.Repository.Get<Models.Wvw.Match>(request);
+        }
+
+        public async void Refresh(string storagePath)
+        {
+            await this.Create(storagePath);
+        }
+    }
+}
---
+++ Gw2Assist.Core/Models/Wvw/Match.cs
@@ -0,0 +1,9 @@
+﻿using Gw2Models = Gw2Assist.Anet.GuildWars2.Api.V2.Models;
+
+namespace Gw2Assist.Core.Models.Wvw
+{
+    public class Match : Gw2Models.Wvw.Match
+    {
+        
+    }
+}
