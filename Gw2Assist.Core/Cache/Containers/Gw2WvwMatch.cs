ac887d3d60d2590fcee052b6d6fac6a59a50e16c
blindwaves <blindwaves@hotmail.com>
2016-05-13 13:07:39 +0800

    GW2 WvW Match cache.

--- Gw2Assist.Core/Cache/Containers/Gw2WvwMatch.cs
+++ Gw2Assist.Core/Cache/Containers/Gw2WvwMatch.cs
@@ -11,24 +11,28 @@ namespace Gw2Assist.Core.Cache.Containers
         public string FileFullPath { get; private set; }
         public string Name { get { return this.GetType().Name; } }
 
-        public async Task Create(string storagePath)
+        private int worldId;
+
+        public Task Create(string storagePath)
         {
             // TODO: Implement settings and grab the world.
-            var settingsWorldId = 1005;
+            this.worldId = 1005;
             // TODO: NULL check for settings, for new app.
 
+            this.Refresh(storagePath);
+
+            return Task.FromResult(false);
+        }
+
+        public async void Refresh(string storagePath)
+        {
             var identifiers = new Dictionary<string, IEnumerable<string>>();
-            identifiers.Add("world", new List<string>() { settingsWorldId.ToString() });
+            identifiers.Add("world", new List<string>() { this.worldId.ToString() });
 
             var request = new Gw2ApiRequest.Wvw.Matches();
             request.Identifiers = identifiers;
 
             this.Contents = await Anet.GuildWars2.Api.V2.Repository.Get<Models.Wvw.Match>(request);
-        }
-
-        public async void Refresh(string storagePath)
-        {
-            await this.Create(storagePath);
         }
     }
 }
---
+++ Gw2Assist.Core/Scheduler/Jobs/Gw2UpdateMatchCache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Gw2Assist.Core.Scheduler.Jobs
+{
+    public class Gw2UpdateMatchCache : Interfaces.IJob
+    {
+        /// <summary>
+        /// Gets the interval to run the job.
+        /// </summary>
+        public TimeSpan Interval { get { return TimeSpan.FromMinutes(5); } }
+        public DateTime? LastRanAt { get; set; }
+        public string Name { get { return this.GetType().Name; } }
+        public Action OnCompleted { get; set; }
+        public object Result { get { return null; } }
+
+        public void Start()
+        {
+            Cache.Repository.Instance.GetContainer<Cache.Containers.Gw2WvwMatch>().Refresh(Cache.Repository.Instance.StoragePath);
+        }
+    }
+}
